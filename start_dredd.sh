99e603e6d0c1b254799821b94943a92bdc29bcad
Yoshihiro Kameda <kameda.sbng@gmail.com>
2020-03-23 16:16:52 +0900

    dredd の実行にdockerを使うように修正

--- start_dredd.sh
+++ start_dredd.sh
@@ -1,2 +1,4 @@
-dredd petstore_simple.yaml localhost:4010 -h "Accept:application/json"
+docker run -it -v $PWD:/api -w /api apiaryio/dredd dredd petstore_simple.yaml host.docker.internal:4010 -h "Accept:application/json"
+
+# dredd petstore_simple.yaml localhost:4010 -h "Accept:application/json"
 
--- start_dredd_v3.sh
+++ start_dredd_v3.sh
@@ -1,2 +1,4 @@
-dredd petstore_simple_v3.v1.yaml localhost:4010 -h "Accept:application/json"
+docker run -it -v $PWD:/api -w /api apiaryio/dredd dredd petstore_simple_v3.v1.yaml host.docker.internal:4010 -h "Accept:application/json"
+
+# dredd petstore_simple_v3.v1.yaml localhost:4010 -h "Accept:application/json"
 

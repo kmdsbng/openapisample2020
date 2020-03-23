596ffd3821adf2ee352ea2b1b9fa309d343e4935
Yoshihiro Kameda <kameda.sbng@gmail.com>
2020-03-23 13:37:22 +0900

    add OpenAPI v3 sample

---
+++ start_dredd_v3.sh
@@ -0,0 +1,2 @@
+dredd petstore_simple_v3.v1.yaml localhost:4010 -h "Accept:application/json"
+
---
+++ start_prism_dynamic_simple_v3.sh
@@ -0,0 +1,6 @@
+docker run --rm \
+  -v ${PWD}:/local \
+  -it \
+  -p 4010:4010 \
+  stoplight/prism:3 mock -h 0.0.0.0 -d /local/petstore_simple_v3.v1.yaml
+
